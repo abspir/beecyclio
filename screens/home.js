65684e3416349ea0f890802fdfd9e71c7fe10fcc
abspir <61014402+abspir@users.noreply.github.com>
2022-04-19 11:27:01 +0100

    new message

--- screens/home.js
+++ screens/home.js
@@ -4,8 +4,9 @@ import { BrowserView, MobileView } from 'react-device-detect';
 //import { View, Text, Button } from 'react-native';
 import qrcode from "../images/qr-to-beecyclio.svg";
 import honeycomb from '../images/honeycomb.svg';
-import CameraImage from '../images/mobile_photos.svg'; 
+import CameraImage from '../images/mobile_photos.svg';
 import { StatusBar } from 'expo-status-bar';
+import axios from 'axios';
 
 const HomeTitle = styled.Text`
     font-size: 1.5rem;
@@ -31,7 +32,45 @@ const Button = styled.Button`
 
 const Home = ({ navigation }) => {
 
+    // const testFunction = async () => {
+    //     try 
+    //     {
+    //         let res = await axios.get('/.netlify/functions/hello');
+    //         console.log(res);
+    //     }
+    //     catch (err)
+    //     {
+    //         console.log(err);
+    //     }
+    // }
 
+    // const testFunction = async () => {
+    //     axios.get('/.netlify/functions/hello')
+    //     .then(function (res) {
+    //         // handle success
+    //         console.log(res);
+    //     })
+    //     .catch(function (err) {
+    //         // handle error
+    //         console.log(err);
+    //     })
+    // }
+
+    let imageDataBase64 = 'Test String';
+
+    const testFunction = (req) => {
+        axios.post('/.netlify/functions/hello', {
+            imageData: req
+        })
+        .then(function (res) {
+            // handle success
+            console.log(res);
+        })
+        .catch(function (err) {
+            // handle error
+            console.log(err);
+        })
+    }
 
     const pressHandler = () => {
         navigation.navigate('Camera');
@@ -39,20 +78,23 @@ const Home = ({ navigation }) => {
 
     return (
         <>
-        <View style={{backgroundImage: `url(${honeycomb})`}}>
-            <HomeTitle>Bzz.. Bzz.. Welcome to Beecyclio</HomeTitle>
-            
-            <Button title="Scan" onPress={pressHandler}/>
-            <BrowserView>
-                <View>
-                    <img style={{width: "auto", maxWidth: "300px"}} alt="qrcode" src={qrcode}/>
-                </View>
-            </BrowserView>
-            <MobileView>
-                Mobile View
-            </MobileView>
-        </View>
-        <StatusBar style="auto"/>
+            <View style={{ backgroundImage: `url(${honeycomb})` }}>
+                <HomeTitle>Bzz.. Bzz.. Welcome to Beecyclio</HomeTitle>
+
+                <Button title="Scan" onPress={pressHandler} />
+
+                <Button title="Test" onPress={testFunction(imageDataBase64)} />
+
+                <BrowserView>
+                    <View>
+                        <img style={{ width: "auto", maxWidth: "300px" }} alt="qrcode" src={qrcode} />
+                    </View>
+                </BrowserView>
+                <MobileView>
+                    Mobile View
+                </MobileView>
+            </View>
+            <StatusBar style="auto" />
         </>
     )
 };
